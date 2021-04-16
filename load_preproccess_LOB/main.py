03484eb8f1ba59e7be84dce38de74deab09a6159
dylanclark <dylan.stanley.clark@gmail.com>
2021-04-16 17:38:48 -0400

    added two election data preproccessing tasks for library of parliment

---
+++ load_preproccess_LOB/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import argparse
+
+
+def create_argument_parser():
+    parser  = argparse.ArgumentParser(description = 'csv-sorter: Sort a CSV file by a particular column.')
+    parser.add_argument('--input-file', nargs = '?', required = True, help = 'A CSV input file.')
+    #parser.add_argument('--column', nargs = '?', required = True, help = 'The column to sort CSV by.')
+    #parser.add_argument('--order', nargs = '?', required = True, choices=['asc', 'dec'], help = 'Ascending or descending')
+    parser.add_argument('--output-file', nargs = '?', required = True, help = 'A CSV output file.')
+    return parser
+
+
+if __name__ == "__main__":
+    parser = create_argument_parser()
+    args = parser.parse_args()
+
+    with open(args.input_file) as csv_file:
+        df = pd.read_csv(csv_file)
+        df = df[df['Election_Type'] == "General"]
+
+    with open(args.output_file, mode="w") as out_file:
+        df.to_csv(out_file)
