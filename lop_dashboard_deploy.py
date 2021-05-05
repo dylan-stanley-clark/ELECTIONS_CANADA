a3b025d9cf4326bdff967f6e2aee69539a2ef97d
dylanclark <dylan.stanley.clark@gmail.com>
2021-05-05 01:41:46 -0400

    Change layout to reduce table crowding

--- lop_dashboard_deploy.py
+++ lop_dashboard_deploy.py
@@ -13,32 +13,33 @@ def main():
 
     election = st.sidebar.selectbox("Election Date", data_base_parms["elections"])
     ridings = data_base_parms[election + " ridings"]
-    constituency = st.sidebar.selectbox("Constituency", ridings, index=len(ridings)-1)
+    
+    summary = st.radio("Page",["Summary","Districts"])
+
 
     # ---SUMMARY PAGE----
-    if constituency == 'Summary':
-        col1, col2,col3 = st.beta_columns(3)
+    if summary == 'Summary':
+        col1, col2= st.beta_columns(2)
+        # with col1:
+        #     title = election + ", provincial summary"
+        #     x = district_tables[title]#[:-46] + """width="700" height="900"></iframe>"""
+        #     st.markdown(x, unsafe_allow_html=True)
         with col1:
-            title = election + ", provincial summary"
-            x = district_tables[title]#[:-46] + """width="700" height="900"></iframe>"""
-            st.markdown(x, unsafe_allow_html=True)
-
-        with col2:
             title = election+", Active Constituencies"
             x = district_tables[title]#[:-46] + """width="2000" height="900"></iframe>"""
             st.markdown(x, unsafe_allow_html=True)
 
-        with col3:
+        with col2:
             title = election + ", Incumbent Elects Seeking Re-Election"
             st.markdown(district_tables[title] , unsafe_allow_html=True)
 
     #Contituency level page
     else:
         #"District Table"
+        constituency = st.selectbox("Constituency", ridings, index=len(ridings) - 1)
         col1, col2, col3 = st.beta_columns(3)
         with col2:
             title = constituency + ", " + election + " Results"
-            st.write(district_tables[title])
             k ='width="600" height="262"></iframe>'
             x = district_tables[title][:-len(k)] + """width="800" height="500"></iframe>"""
 
