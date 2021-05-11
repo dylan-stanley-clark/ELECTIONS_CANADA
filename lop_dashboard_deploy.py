b754e857adaa1f5e48e32f1f693551af2395f601
dylanclark <dylan.stanley.clark@gmail.com>
2021-05-11 15:15:54 -0400

    Added code to load and link incumbent resource and candidate table from LOP

--- lop_dashboard_deploy.py
+++ lop_dashboard_deploy.py
@@ -19,7 +19,7 @@ def main():
     # ---Election SUMMARY PAGE----
     if page == 'Summary':
         info = st.sidebar.radio('Election Info',
-                                options=['Districts', 'Incumbents', "Vote Summary", "Conflicts", "Verification"])
+                                options=['Districts', 'Incumbents', "Vote Summary"])
 
         col1, col2 = st.beta_columns(2)
         with col1:
@@ -44,15 +44,6 @@ def main():
                     st.markdown(district_tables[title], unsafe_allow_html=True)
                 except:
                     st.write("Chart not created yet")
-
-            if info == "Conflicts":
-                title = election + ", conflict summary"
-                x = """<iframe title="Conflicts With Canadian Election Database" aria-label="chart" id="datawrapper-chart-JZdUB" src="https://datawrapper.dwcdn.net/JZdUB/2/" scrolling="no" frameborder="0" style="border: none;" width="400" height="315"></iframe>"""
-
-                st.markdown(x, unsafe_allow_html=True)
-
-                x2 = """<iframe title="Existing Conflicts Between LOP &amp;amp; CED" aria-label="table" id="datawrapper-chart-YpJ5o" src="https://datawrapper.dwcdn.net/YpJ5o/1/" scrolling="no" frameborder="0" style="border: none;" width="600" height="720"></iframe>"""
-                st.markdown(x2, unsafe_allow_html=True)
 
     # Contituency level page
     else:
