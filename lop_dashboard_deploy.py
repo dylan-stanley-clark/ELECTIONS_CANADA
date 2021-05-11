cc851f06411541ee57dbabb0d4b0f463f95dc7bf
dylanclark <dylan.stanley.clark@gmail.com>
2021-05-11 13:30:02 -0400

    added exception for default district index value

--- lop_dashboard_deploy.py
+++ lop_dashboard_deploy.py
@@ -73,8 +73,10 @@ def main():
 
         session_state = SessionState.get(first_query_params=query_params)
         first_query_params = session_state.first_query_params
-
-        default_index = eval(first_query_params["district"][0]) if "district" in app_state else 0
+        if "district" in app_state:
+            default_index = eval(first_query_params["district"][0])
+        else:
+            default_index = 0
 
 
         col1, col2 = st.beta_columns(2)
