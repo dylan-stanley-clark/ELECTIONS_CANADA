583723a98b84424a5e514180cee3c9bf660339e8
dylanclark <dylan.stanley.clark@gmail.com>
2021-07-13 14:13:21 -0400

    I added s3 bucket test

--- S3/s3_example.py
+++ S3/s3_example.py
@@ -22,7 +22,7 @@ st.write(df_cabinet)
 
 save = st.button("save")
 if save:
-    #df.to_csv("s3://polemics/test.csv")
-    with fs.open('polemics/lop-cabinet', 'wb') as f:
-        f.write(df_cabinet)
+    df_cabinet.to_csv("s3://polemics/test.csv")
+    # with fs.open('polemics/lop-cabinet', 'wb') as f:
+    #     f.write(df_cabinet)
     st.balloons()
