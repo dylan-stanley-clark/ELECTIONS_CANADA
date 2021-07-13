2cfb59c930c1c8ed5234fbacef099f4ba8764fc5
dylanclark <dylan.stanley.clark@gmail.com>
2021-07-13 13:41:59 -0400

    I added s3 bucket test

--- S3/s3_example.py
+++ S3/s3_example.py
@@ -7,7 +7,6 @@ AWS_S3_BUCKET = "polemics"
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
-
 # Create connection object.
 # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
 fs = s3fs.S3FileSystem(anon=False)
@@ -19,7 +18,7 @@ def read_file(filename):
     with fs.open(filename) as f:
         return pd.read_csv(f)
 
-df = read_file('3://polemics/roles.csv')
+df = read_file('s3://polemics/roles.csv')
 df = df[(df['parliament'] == 43) & (df['status'] == 'active')] # lets just look at 43
 df_cabinet = df[df['Role']== "Minister"] #excludes PM
 
