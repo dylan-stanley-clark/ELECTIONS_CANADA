2a76a22f696a70a27ff08a08bd9608114a3894a8
dylanclark <dylan.stanley.clark@gmail.com>
2021-07-13 13:56:19 -0400

    I added s3 bucket test

--- S3/s3_example.py
+++ S3/s3_example.py
@@ -14,15 +14,20 @@ fs = s3fs.S3FileSystem(anon=False)
 # Retrieve file contents.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
-def read_file(filename):
-    with fs.open(filename) as f:
-        df = pd.read_csv(filename)
-    return df
+def from_s3(s3_uri:str) -> pd.DataFrame:
+    client = boto3.client("s3")
+    parsed_s3 = urlparse(s3_uri)
+
+    obj = client.get_object(Bucket=parsed_s3.netloc, Key=parsed_s3.path)
+    bytes_ = BytesIO(obj["Body"].read())
+    return pd.read_csv(bytes_)
+
+df = from_s3("https://polemics.s3.ca-central-1.amazonaws.com/roles.csv")
+
 # def read_file(filename):
 #     with fs.open(filename) as f:
 #         return pd.read_csv(f)
-
-df = read_file('s3://polemics/roles.csv')
+#df = read_file('s3://polemics/roles.csv')
 
 df = df[(df['parliament'] == 43) & (df['status'] == 'active')] # lets just look at 43
 df_cabinet = df[df['Role']== "Minister"] #excludes PM
@@ -30,12 +35,3 @@ df_cabinet = df[df['Role']== "Minister"] #excludes PM
 save = st.button("save")
 if save:
     st.write(df_cabinet)
-    # df_cabinet.to_csv(
-    #     f"s3://{AWS_S3_BUCKET}/cabinet.csv",
-    #     index=False,
-    #     storage_options={
-    #         "key": AWS_ACCESS_KEY_ID,
-    #         "secret": AWS_SECRET_ACCESS_KEY
-    #         #"token": AWS_SESSION_TOKEN,
-    #     },
-    # )
